fb9cac184f50a37db45ef90cce81c27f444f2d56
Michele Finotto <m@finotto.org>
2018-02-21 10:38:06 +0000

    Initial release

---
+++ README.md
@@ -0,0 +1,49 @@
+## WrappedJSONFormatter
+
+This [logrus](https://github.com/sirupsen/logrus) formatter allows you to wrap you JSON logs into a first level element named after the field `kind`.
+
+An example with the standard `JSONFormatter` would be:
+
+```json
+{"animal":"walrus","level":"info","msg":"A giant walrus appears!",
+"size":10,"time":"2014-03-10 19:57:38.562500591 -0400 EDT"}
+```
+
+With `WrappedJSONFormatter`, you could pass an extra field like this:
+
+```go
+log = log.WithField("kind", "wrapped_key_name")
+```
+
+And the output would become:
+
+```json
+{"wrapped_key_name": {"animal":"walrus","level":"info","msg":"A giant walrus appears!",
+"size":10,"time":"2014-03-10 19:57:38.562500591 -0400 EDT"}}
+```
+
+Example:
+
+```go
+package main
+
+import (
+	"os"
+	"fmt"
+	"flag"
+
+	log "github.com/sirupsen/logrus"
+	wrapped "github.com/michele/logrus-wrapped-json"
+)
+
+func init() {
+  log.SetFormatter(&wrapped.WrappedJSONFormatter{})
+}
+
+func main() {
+    httpLog := log.WithField("kind", "response_log")
+    dbLog := log.WithField("kind", "sql_log")
+
+    ...
+}
+```
---
+++ formatter.go
@@ -0,0 +1,110 @@
+package wrapped
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type wrapper map[string]logrus.Fields
+
+const defaultTimestampFormat = time.RFC3339
+
+const (
+	fieldKeyMsg   = "msg"
+	fieldKeyLevel = "level"
+	fieldKeyTime  = "time"
+	defaultKind   = "log"
+)
+
+type WrappedJSONFormatter struct {
+	// TimestampFormat sets the format used for marshaling timestamps.
+	TimestampFormat string
+
+	// DisableTimestamp allows disabling automatic timestamps in output
+	DisableTimestamp bool
+
+	// FieldMap allows users to customize the names of keys for various fields.
+	// As an example:
+	// formatter := &JSONFormatter{
+	//   	FieldMap: FieldMap{
+	// 		 FieldKeyTime: "@timestamp",
+	// 		 FieldKeyLevel: "@level",
+	// 		 FieldKeyLevel: "@message",
+	//    },
+	// }
+	FieldMap FieldMap
+}
+
+type fieldKey string
+
+// FieldMap allows customization of the key names for default fields.
+type FieldMap map[fieldKey]string
+
+func (f *WrappedJSONFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	wrp := wrapper{}
+	var wrapKey string
+	kind, ok := entry.Data["kind"]
+	if ok {
+		wrapKey, ok = kind.(string)
+		if !ok {
+			wrapKey = defaultKind
+		}
+		delete(entry.Data, "kind")
+	} else {
+		wrapKey = defaultKind
+	}
+	data := make(logrus.Fields, len(entry.Data)+3)
+	for k, v := range entry.Data {
+		switch v := v.(type) {
+		case error:
+			// Otherwise errors are ignored by `encoding/json`
+			// https://github.com/sirupsen/logrus/issues/137
+			data[k] = v.Error()
+		default:
+			data[k] = v
+		}
+	}
+	prefixFieldClashes(data)
+
+	timestampFormat := f.TimestampFormat
+	if timestampFormat == "" {
+		timestampFormat = defaultTimestampFormat
+	}
+
+	if !f.DisableTimestamp {
+		data[f.FieldMap.resolve(fieldKeyTime)] = entry.Time.Format(timestampFormat)
+	}
+	data[f.FieldMap.resolve(fieldKeyMsg)] = entry.Message
+	data[f.FieldMap.resolve(fieldKeyLevel)] = entry.Level.String()
+	wrp[wrapKey] = data
+	serialized, err := json.Marshal(wrp)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to marshal fields to JSON, %v", err)
+	}
+	return append(serialized, '\n'), nil
+}
+
+func prefixFieldClashes(data logrus.Fields) {
+	if t, ok := data["time"]; ok {
+		data["fields.time"] = t
+	}
+
+	if m, ok := data["msg"]; ok {
+		data["fields.msg"] = m
+	}
+
+	if l, ok := data["level"]; ok {
+		data["fields.level"] = l
+	}
+}
+
+func (f FieldMap) resolve(key fieldKey) string {
+	if k, ok := f[key]; ok {
+		return k
+	}
+
+	return string(key)
+}
